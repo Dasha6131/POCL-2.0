2ebe3950fe5a4424ff80c4a1115f7dc7baf30c38
Margarita Onuchina <116965536+Margarita-Onuchina@users.noreply.github.com>
2023-04-25 20:05:36 +0500

    UPDATE: import app to test file

--- test_sample.py
+++ test_sample.py
@@ -1,28 +1,5 @@
 '''Тесты для проекта'''
-from transformers import pipeline
-import streamlit as st
-
-classifier = pipeline("zero-shot-classification",
-                      model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
-
-text_input = st.text_input('Введите текст')
-
-
-def classify(text=None):
-    """Classify the given text using a classifier"""
-    if text is None:
-        text = text_input
-    candidate_labels = ["Учетная запись", "РПД",
-                        "Учебные планы", "Личный кабинет"]
-    output = classifier(text, candidate_labels,
-                        multi_label=False, use_fast=False)
-    return output["labels"][0], output["scores"][0]
-
-
-submit = st.button('Отправить')
-
-if submit:
-    st.write(classify())
+from app import classify
 
 
 def test_classify():
